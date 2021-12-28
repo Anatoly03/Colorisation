921976cace86335bb7b7dc120a85be0bc7c43a3c
Anatoly03 <anatoly.wein@gmail.com>
2021-12-28 20:58:47 +0100

    fix: eval function

--- src/js/app.ts
+++ src/js/app.ts
@@ -14,44 +14,75 @@
 
 export let DEBUG_MODE: boolean
 
-export let width: number
-export let height: number
-export let canvas: HTMLCanvasElement
 export let ctx: CanvasRenderingContext2D
 export let keys: { [key: string]: boolean; } = {}
 
-let lastCalledTime = Date.now();
-let delta = 1;
-export let FPS = 0;
+let image = new Image()
+let m_func : Function = (r : number, g: number, b: number, a: number) => [r, g, b, a]
 
 export let app = {
+	formula: <HTMLTextAreaElement>null,
+	canvas: <HTMLCanvasElement>null,
+	link: <HTMLLinkElement>null,
+	assets: <HTMLImageElement[]>[],
 	setup() {
-		canvas = <HTMLCanvasElement>document.getElementById('canvas')
+		this.formula = document.getElementById('formula')
+		this.canvas = document.getElementById('canvas')
+		this.link = document.getElementById('executor')
+
+		this.formula.value = 'let rn = (r + g + b) / 3\nlet gn = (r + g + b) / 3\nlet bn = (r + g + b) / 3'
+
+		this.link.onclick = () => {
+			m_func = new Function('"use strict";let r=arguments[0];let g=arguments[1];let b=arguments[2];let a=arguments[3];' + this.formula.value)
+		}
+
 		this.update()
-		this.updateFPS()
 		this.addEventLiteners()
-		//game.init()
-		//lobby.init()
+
+		image.src = 'assets/basic.png'
+
+		//mod_image.data = image.data
 	},
 	update() {
-		width = canvas.width = window.innerWidth
-		height = canvas.height = window.innerHeight
-		ctx = canvas.getContext("2d")
+		this.canvas.width = 800 //window.innerWidth
+		this.canvas.height = 200 // window.innerHeight
+
+		ctx = this.canvas.getContext("2d")
 		ctx.imageSmoothingEnabled = false
 
 		// Black Background
 		ctx.fillStyle = 'black'
-		ctx.fillRect(0, 0, width, height)
-	},
-	updateFPS() {
-		FPS = 1 / delta
-		setTimeout(() => {
-			this.updateFPS()
-		}, 100)
+		ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+		// Image
+
+		ctx.drawImage(image, 30, 30, 160, 160)
+		ctx.drawImage(image, 200, 30, 20, 20)
+		//ctx.drawImage(mod_image, 230, 30, 160, 160)
+		//ctx.drawImage(mod_image, 300, 30, 20, 20)
+
+		const imageData = ctx.getImageData(200, 30, 20, 20)
+		const data = imageData.data;
+
+		for (var i = 0; i < data.length; i += 4) {
+			/*data[i] = 255 - data[i]
+			data[i + 1] = 255 - data[i + 1]
+			data[i + 2] = 255 - data[i + 2]*/
+
+			[data[i], data[i + 1], data[i + 2], data[i + 3]] = m_func.call(data[i], data[i + 1], data[i + 2], data[i + 3])
+		}
+
+		ctx.putImageData(imageData, 230, 30);
+
+		for (var i = 0; i < data.length; i += 4) {
+			ctx.fillStyle = `rgba(${data[i]}, ${data[i + 1]}, ${data[i + 2]}, ${data[i + 3] / 255})`
+			ctx.fillRect(
+				260 + 8 * ((i / 4) % imageData.width),
+				30 + 8 * Math.floor(i / 4 / imageData.width),
+				8, 8)
+		}
 	},
 	gameLoop() {
-		delta = (Date.now() - lastCalledTime) / 1000;
-		lastCalledTime = Date.now();
 		requestAnimationFrame(this.gameLoop.bind(this))
 		this.update()
 		//game.update()
@@ -80,20 +111,6 @@ export let app = {
 			//lobby.onClick(event)
 		})
 
-		/*document.addEventListener('mousedown', event => {
-			//
-		})
-
-		document.addEventListener('mouseup', event => {
-			//
-		})
-
-		document.addEventListener('mousemove', event => {
-			//game.onClick(event)
-			game.onMouseMove(event)
-		})*/
-
-
 	}
 }
 
@@ -101,3 +118,9 @@ window.onload = () => {
 	app.setup()
 	app.gameLoop()
 }
+
+// 
+const getColorIndicesForCoord = (x: number, y: number, width: number) => {
+	const red = y * (width * 4) + x * 4;
+	return [red, red + 1, red + 2, red + 3];
+};
